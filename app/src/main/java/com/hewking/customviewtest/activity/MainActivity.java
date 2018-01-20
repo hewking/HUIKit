68335b88b1c367ff3aa92d6499040e901a91632c
hewking <604910070qq.com>
2018-01-20 20:39:49 +0800

    add

--- app/src/main/java/com/hewking/customviewtest/activity/MainActivity.java
+++ app/src/main/java/com/hewking/customviewtest/activity/MainActivity.java
@@ -2,10 +2,8 @@ package com.hewking.customviewtest.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.hewking.customviewtest.R;
-import com.hewking.customviewtest.stickytop.StickyTopLayout;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,14 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        DialogFragment dialogFragment = new ImageDialogFragment();
 //        dialogFragment.show(getSupportFragmentManager(),"mainactivity");
-        findViewById(R.id.btn_sure).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                StickyTopLayout topLayout = (StickyTopLayout) findViewById(R.id.sticktop);
-                expand = !expand;
-                topLayout.top(expand);
-            }
-        });
+//        findViewById(R.id.btn_sure).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                StickyTopLayout topLayout = (StickyTopLayout) findViewById(R.id.sticktop);
+//                expand = !expand;
+//                topLayout.top(expand);
+//            }
+//        });
     }
 
     boolean expand = false;
