0dc2f43e6cbb2aa34bfbb235c7b6132a97e71244
hewking <604910070qq.com>
2018-12-09 17:03:58 +0800

    add ViewDragLayout,TinderStackLayout

--- app/src/main/java/com/hewking/custom/tinderstack/TinderStackLayout.java
+++ app/src/main/java/com/hewking/custom/tinderstack/TinderStackLayout.java
@@ -1,11 +1,15 @@
 package com.hewking.custom.tinderstack;
 
 import android.content.Context;
+import android.graphics.Point;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.hewking.custom.util.ViewExKt;
 
 /**
  * Created by hewking on 2017/3/30.
@@ -13,7 +17,13 @@ import android.view.ViewGroup;
 
 public class TinderStackLayout extends ViewGroup {
 
+    public static final float DEFAULT_SCALE = 0.1f;
+    public static final int DEFAULT_OFFSET = 5;//dp
+    public static final int DEFAULT_MARGIN = 10;//dp
+
     private ViewDragHelper mDragHelper;
+
+    private Point mPoint = new Point();
 
     public TinderStackLayout(Context context) {
         this(context,null);
@@ -29,19 +39,76 @@ public class TinderStackLayout extends ViewGroup {
     }
 
     private void init() {
-        mDragHelper = ViewDragHelper.create(this, new ViewDragHelper.Callback() {
+        mDragHelper = ViewDragHelper.create(this,1.0f, new ViewDragHelper.Callback() {
             @Override
             public boolean tryCaptureView(View child, int pointerId) {
-                return false;
+                return indexOfChild(child) == getChildCount() -1;
+            }
+
+            @Override
+            public int clampViewPositionHorizontal(@NonNull View child, int left, int dx) {
+                return left;
+            }
+
+            @Override
+            public int clampViewPositionVertical(@NonNull View child, int top, int dy) {
+                return top;
+            }
+
+            @Override
+            public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
+                if (indexOfChild(releasedChild) == getChildCount() -1) {
+                    mDragHelper.settleCapturedViewAt(mPoint.x,mPoint.y);
+                    invalidate();
+                }
+
+            }
+
+            @Override
+            public void onViewPositionChanged(@NonNull View changedView, int left, int top, int dx, int dy) {
+
+            }
+
+            @Override
+            public void onEdgeDragStarted(int edgeFlags, int pointerId) {
+                super.onEdgeDragStarted(edgeFlags, pointerId);
+            }
+
+            @Override
+            public int getViewHorizontalDragRange(@NonNull View child) {
+                return super.getViewHorizontalDragRange(child);
+            }
+
+            @Override
+            public int getViewVerticalDragRange(@NonNull View child) {
+                return super.getViewVerticalDragRange(child);
             }
         });
-
-//        mDragHelper.
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        float scale = 1f;
+        for (int i = getChildCount() -1 ; i > 0 ; i --) {
+            View child = getChildAt(i);
+            float scaleValue = scale - DEFAULT_SCALE * (getChildCount() - 1) * i;
+            child.setScaleX(scaleValue);
+            child.setScaleY(scaleValue);
 
+            int margin = ViewExKt.dp2px(this,DEFAULT_MARGIN);
+            int offset= ViewExKt.dp2px(this,DEFAULT_OFFSET);
+            int scaledWidth = (int) (child.getMeasuredWidth() * scaleValue);
+            int scaledHeight = (int) (child.getMeasuredHeight() * scaleValue);
+            int childPadding = (getMeasuredWidth() - scaledWidth) / 2;
+            child.layout(childPadding
+                    ,t + margin + offset
+                    ,childPadding + scaledWidth
+                    ,t + margin + scaledHeight + offset);
+            if (i == getChildCount() -1) {
+                mPoint.x = childPadding;
+                mPoint.y = childPadding + scaledWidth;
+            }
+        }
     }
 
     @Override
@@ -70,5 +137,11 @@ public class TinderStackLayout extends ViewGroup {
         super.onSizeChanged(w, h, oldw, oldh);
     }
 
-
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mDragHelper.continueSettling(true)) {
+            postInvalidate();
+        }
+    }
 }
--- app/src/main/java/com/hewking/custom/viewdrager/ViewDragLayout.kt
+++ app/src/main/java/com/hewking/custom/viewdrager/ViewDragLayout.kt
@@ -1,6 +1,7 @@
 package com.hewking.custom.viewdrager
 
 import android.content.Context
+import android.graphics.Point
 import android.support.v4.widget.ViewDragHelper
 import android.util.AttributeSet
 import android.view.MotionEvent
@@ -20,12 +21,17 @@ import android.widget.FrameLayout
 class ViewDragLayout(ctx : Context, attrs : AttributeSet) : FrameLayout(ctx,attrs) {
 
     private lateinit var mDragHelper : ViewDragHelper
+    private var mDragView : View? = null
+    private var mAutoBackView : View? = null
+    private var mEdgeTouchView : View? = null
+
+    private val mPoint = Point()
 
     init {
         mDragHelper = ViewDragHelper.create(this,1.0f,object : ViewDragHelper.Callback(){
             override fun tryCaptureView(p0: View, p1: Int): Boolean {
 
-                return true
+                return p0 == mDragView || p0 == mAutoBackView
             }
 
             override fun clampViewPositionHorizontal(child: View, left: Int, dx: Int): Int {
@@ -39,7 +45,32 @@ class ViewDragLayout(ctx : Context, attrs : AttributeSet) : FrameLayout(ctx,attr
             override fun clampViewPositionVertical(child: View, top: Int, dy: Int): Int {
                 return top
             }
+
+            override fun onViewReleased(releasedChild: View, xvel: Float, yvel: Float) {
+                if (releasedChild == mAutoBackView) {
+                    mDragHelper.settleCapturedViewAt(mPoint.x,mPoint.y)
+                    invalidate()
+                }
+            }
+
+            override fun onEdgeDragStarted(edgeFlags: Int, pointerId: Int) {
+                mEdgeTouchView?:return
+                mDragHelper.captureChildView(mEdgeTouchView!!,pointerId)
+            }
+
+            /**
+             * 一下两个方法，是clickable = true,子view消耗事件的情况
+             * 如果这时候，需要一下判断范围是否capture
+             */
+            override fun getViewHorizontalDragRange(child: View): Int {
+                return measuredWidth - child.measuredWidth
+            }
+
+            override fun getViewVerticalDragRange(child: View): Int {
+                return measuredHeight - child.measuredHeight
+            }
         })
+        mDragHelper.setEdgeTrackingEnabled(ViewDragHelper.EDGE_LEFT.or(ViewDragHelper.EDGE_RIGHT))
 
     }
 
@@ -51,5 +82,35 @@ class ViewDragLayout(ctx : Context, attrs : AttributeSet) : FrameLayout(ctx,attr
         mDragHelper.processTouchEvent(event)
         return true
     }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        mAutoBackView?.let {
+            mPoint.x = it.left
+            mPoint.y = it.top
+        }
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        ensureChildView()
+        if(childCount < 3) {
+            return
+        }
+        mDragView= getChildAt(0)
+        mAutoBackView = getChildAt(1)
+        mEdgeTouchView = getChildAt(2)
+    }
+
+    private fun ensureChildView() {
+
+    }
+
+    override fun computeScroll() {
+        if (mDragHelper.continueSettling(true)){
+            postInvalidate()
+        }
+    }
+
 
 }
