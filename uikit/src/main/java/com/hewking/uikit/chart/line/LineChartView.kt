a5e175a9a40c4c6fffbafee8a158a3c086c8a09a
jianhao <chengjianhao@fcbox.com>
2020-11-07 13:41:11 +0800

    update

---
+++ uikit/src/main/java/com/hewking/uikit/chart/line/LineChartView.kt
@@ -0,0 +1,25 @@
+package com.hewking.uikit.chart.line
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * @Description: 折线图带shader和动画
+ * @Author: jianhao
+ * @Date:   2020/10/15 14:23
+ * @License: Copyright Since 2020 Hive Box Technology. All rights reserved.
+ * @Notice: This content is limited to the internal circulation of Hive Box, and it is prohibited to leak or used for other commercial purposes.
+ */
+class LineChartView(ctx: Context, attrs: AttributeSet): View(ctx,attrs) {
+
+  init {
+
+  }
+
+  override fun onDraw(canvas: Canvas?) {
+    super.onDraw(canvas)
+  }
+
+}
